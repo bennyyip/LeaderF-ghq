6f3c7f8385d04b8072329f2cab3a19cf948ece32
tamago324 <tamago_pad@yahoo.co.jp>
2020-01-31 18:30:16 +0900

    Add highlight

--- autoload/leaderf/python/ghqExpl.py
+++ autoload/leaderf/python/ghqExpl.py
@@ -23,7 +23,21 @@ class GhqExplorer(Explorer):
             return self.getFreshContent()
 
     def getFreshContent(self, *args, **kwargs):
-        self._content = lfEval("system('ghq list')").split()
+        repos = lfEval("system('ghq list')").split()
+
+        # github.com/tamago324/LeaderF-ghq
+        # <---------/-------------------->
+        contents = [r.split('/', 1) for r in repos]
+
+        max_repo_len = max(int(lfEval("strdisplaywidth('%s')" % escQuote(item[1]))) for item in contents)
+
+        lines = []
+
+        for service, repo in contents:
+            space_num = max_repo_len - int(lfEval("strdisplaywidth('%s')" % escQuote(repo)))
+            lines.append('%s%s "%s"' % (repo, " " * space_num, service))
+
+        self._content = lines
 
         return self._content
 
@@ -32,6 +46,9 @@ class GhqExplorer(Explorer):
 
     def getStlCurDir(self):
         return escQuote(lfEncode(os.getcwd()))
+
+    def supportsNameOnly(self):
+        return True
 
 
 #*****************************************************
@@ -54,16 +71,45 @@ class GhqExplManager(Manager):
         cmd = lfEval("g:Lf_GhqAcceptSelectionCmd")
         if cmd == '':
             cmd = 'lcd'
-        repo = args[0]
-        lfCmd(cmd + ' ' + os.path.join(_root, repo))
+        repo = self._getDigest(args[0], 1)
+        service = self._getDigest(args[0], 2)
+        lfCmd(cmd + ' ' + os.path.join(_root, os.path.join(service, repo)))
 
     def _getDigest(self, line, mode):
+        """
+        specify what part in the line to be processed and highlighted
+        Args:
+            mode: 0, return the full path
+                  1, return the name only
+                  2, return the directory name
+        """
         if not line:
             return ''
-        return line
+        if mode == 0:
+            return line
+        elif mode == 1:
+            start_pos = line.find(' "')
+            return line[:start_pos].rstrip()
+        else:
+            start_pos = line.find(' "')
+            return line[start_pos+2:-1]
 
     def _getDigestStartPos(self, line, mode):
-        return 0
+        """
+        specify what part in the line to be processed and highlighted
+        Args:
+            mode: 0, return the full path
+                  1, return the name only
+                  2, return the directory name
+        """
+        if not line:
+            return 0
+
+        if mode == 2:
+            start_pos = line.find(' "')
+            return lfBytesLen(line[:start_pos+2])
+        else:
+            return 0
 
     def _createHelp(self):
         help = []
@@ -74,6 +120,19 @@ class GhqExplManager(Manager):
         help.append('" <F5> : refresh the cache')
         help.append('" ---------------------------------------------------------')
         return help
+
+    def _afterEnter(self):
+        super(GhqExplManager, self)._afterEnter()
+        if self._getInstance().getWinPos() == "popup":
+            lfCmd(
+                """call win_execute(%d, 'let matchid = matchadd(''Lf_hl_ghqServiceName'', ''\s\+\zs".\+'')')"""
+                % self._getInstance().getPopupWinId()
+            )
+            id = int(lfEval("matchid"))
+            self._match_ids.append(id)
+        else:
+            id = int(lfEval("matchadd('Lf_hl_ghqServiceName', '\s\+\zs"".\+')"))
+            self._match_ids.append(id)
 
 
 #*****************************************************
